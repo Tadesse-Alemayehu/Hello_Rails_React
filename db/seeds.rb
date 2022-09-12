296bbc6039a9a9b5ea02aef780a36583e2468999
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-12 23:36:29 +0300

    Create: migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do |i|
+Message.create!(message: "this is random message #{i}")
+end
