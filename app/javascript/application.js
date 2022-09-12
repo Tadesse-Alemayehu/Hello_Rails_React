b88d83c5b970cc6a94acacef56868462d680508a
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-13 02:05:39 +0300

    Configure routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import Greeting from "./greeting";
-
-function App(props) {
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function Root() {
   return (
-    <Provider store={store}>
-      <h1>Hello World!</h1>
-      <Greeting />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/greeting" element={<Greeting />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById("root"));
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  get '/greeting', to: 'messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
